29e93074c65995b5db053c3db9d05fa5c492ffa6
Steleac Raul <raul.steleac@student.upt.ro>
2017-04-08 19:06:34 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -6,107 +6,140 @@
 
 //DECLARAREA VARIABILELOR GLOBALE
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-int flag;
-pthread_mutex_t mutex;
-pthread_cond_t flag_cv;
-struct container *init;
-int soc;
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+int flag1,flag2;
+pthread_cond_t flag1_cv,flag2_cv;
+struct container* init;
+int socr,soct;
 socklen_t receiverlen;
+socklen_t transmitterlen;
 char buffer[255];
 struct sockaddr_in receiver;
+struct sockaddr_in transmitter;
 //INITIALIZAREA VARIABILELOR GLOBALE
 
 void initial()
 {
       init=(struct container *)malloc(sizeof(struct container));
-      init->directie='B';
-      init->vt.viteza=30;
+      init->directie='F';
+      init->viteza=70;
       pthread_mutex_init(&mutex,NULL);
-      pthread_cond_init(&flag_cv,NULL);
-      flag=0;
+      pthread_mutex_init(&mutex2,NULL);
+      pthread_cond_init(&flag1_cv,NULL);
+      pthread_cond_init(&flag2_cv,NULL);
+      flag1=0;
+      flag2=0;
+      initializesocket(&socr,receiverlen,receiver);
+      broadcastpermissioner(&socr);
+      initializesocket(&soct,transmitterlen,transmitter);
+      broadcastpermissioner(&soct);
 }
 //O AFISARE
 void* afis()
 {
 
-     printf("%c %d\n",init->directie,init->vt.viteza);
+     printf("%c %d %d\n",init->directie,init->viteza,init->timp);
      return 0;
 }
 
 // CLIENTUL UDP , AICI SE FACE COMUNICAREA SI STOCAREA DATELOR
-void* udpclient(struct container *init)
+void* udpclientreader(struct container *init)
 {
-initializesocket(&soc,receiverlen,receiver);
-broadcastpermissioner(&soc);
-letssend(&soc,receiverlen);
+
+letssend(&socr,receiverlen);
+
+gethostip(socr);
 		while(1)
 		{
-      letsreceive(&soc,receiverlen,buffer,receiver);
-      setbufferul(buffer,init);
+
+      letsreceive(&socr,receiverlen,buffer,receiver);
+      setcontainer(buffer,init);
 
 //AICI TRANSMIT SEMNALUL CU SCOPA DE A ANUNTA CA S-A MODIFICAT MEMORIA , FLAGUL CV
 
 	     pthread_mutex_lock(&mutex);
-	     flag=1;
-	     pthread_cond_signal(&flag_cv);
+	     flag1=1;
+	     pthread_cond_signal(&flag1_cv);
 	     pthread_mutex_unlock(&mutex);
+  printf("Clinetul asteapta o comanda :\n" );
 		}
-	close(soc);
+	close(socr);
 	return 0;
 
+}
+void* udpclienttransmitter()
+{
+      int i=0;
+      while(1)
+      {
+        letssend(&soct,transmitterlen);
+         sleep(2);
+      }
 }
 // AICI V-A FI INTRODUSA FUNCTIA DE MISCARE A MOTOARELOR
 void *motors(struct container *init)
 {
-	char miscareAnterioara;
-	int vitezaAnterioara;
+	//char miscareAnterioara;
+	//int vitezaAnterioara;
 	while(1)
 	{
-	
-		if(init->directie!='S')
+
+	/*	if(init->directie!='S')
 		{
 			 miscareAnterioara=init->directie;
-			 vitezaAnterioara=init->vt.viteza;
+			 vitezaAnterioara=init->viteza;
 		}
+    */
 	pthread_mutex_lock(&mutex);
 //AICI SE ASTEAPTA SEMNALUL
-	while(flag==0)
+	while(flag1==0)
 	{
-		pthread_cond_wait(&flag_cv,&mutex);
+		pthread_cond_wait(&flag1_cv,&mutex);
 	}
 	pthread_mutex_unlock(&mutex);
 //......AICI VA FI APELATA FUNCTIA DE MISCARE A MOTOARELOR , threadul asteapta ca memoria sa fi schimbata si apoi va anunta functia
 // DACA MEMORIA NU S-A SCHIMBAT FUNCTIA VA CONTINUA CU ACEEASI DEIRECTIE
-	
-	if(flag)
+
+	if(flag1)
 	{/*
 	switch(init->directie)
 	{
 		case 'S': printf("Stop!\n");
 				  drive_s();
-				  
-				  delay((init->vt.timp)*1000);
+
+				  delay((init->timp)*1000);
 				  if(miscareAnterioara=='F')
 				  	drive_f(vitezaAnterioara);
 				  else
 				  	drive_b(vitezaAnterioara);
 				  break;
 		case 'F': printf("Drive Forward!\n");
-				  drive_f(init->vt.viteza);
+				  drive_f(init->viteza);
+          sleep(init->timp);
+          drive_s();
 				  break;
 		case 'L': printf("Drive Left!\n");
-				  drive_l(init->vt.viteza);
+				  drive_l(init->viteza);
+          sleep(init->timp);
+          drive_s();
 				  break;
 		case 'R': printf("Drive Right!\n");
-				  drive_r(init->vt.viteza);
+				  drive_r(init->viteza);
+          sleep(init->timp);
+          drive_s();
 				  break;
 		case 'B': printf("Drive Back!\n");
-				  drive_b(init->vt.viteza);
+				  drive_b(init->viteza);
+          sleep(init->timp);
+          drive_s();
 				  break;
 		}
 		*/
-	printf("%c %d\n",init->directie,init->vt.viteza);
-	flag=0;
+    if(init->directie=='B' || init->directie=='F')
+	     printf("%c %d %d\n",init->directie,init->viteza,init->timp);
+    else
+      printf("%c %d\n",init->directie,init->timp );
+	flag1=0;
 	}
 	}
 	return 0;
@@ -124,11 +157,12 @@ void threadcreator()
     printf("PWM  is running! Press CTRL+C to quit.\n");
     */
     //initializare thread-uri
-    
+
 	initial();
-	pthread_t t1,t2;
-	pthread_create(&t1,NULL,(void*)udpclient,init);
-	pthread_create(&t2,NULL,(void*)motors,init);
+	pthread_t t1,t2,t3;
+	pthread_create(&t1,NULL,(void*)udpclientreader,init);
+  pthread_create(&t2,NULL,(void*)udpclienttransmitter,NULL);
+  pthread_create(&t3,NULL,(void*)motors,init);
 	pthread_join(t1,NULL);
 	pthread_join(t2,NULL);
 
--- milestone1server.c
+++ milestone1server.c
@@ -12,58 +12,128 @@
 #include <netinet/in.h>
 #include <net/if.h>
 #include <arpa/inet.h>
-
+#include <pthread.h>
 void err(char *msg)
 {
 	perror(msg);
 	exit(1);
-}	
+}
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int flag1,flag2;
+pthread_cond_t flag1_cv,flag2_cv;
+
+void* udpclientreader()
+{
+	int soc;
+	struct sockaddr_in receiver;
+	socklen_t receiverlen;
+	if((soc=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP))==-1)
+				err("Nu merge socket");
+	//setare conexiune , unde trimitem datele
+	memset((char*)&receiver,0,sizeof(receiver));
+	receiver.sin_family=AF_INET;
+	receiver.sin_port=htons(5000);
+	receiverlen=sizeof(receiver);
+	inet_pton(AF_INET, "255.255.255.255", &receiver.sin_addr);
+	if(bind(soc,(struct sockaddr*)&receiver,sizeof(receiver))==-1)
+			err("Nu merge bind");
+
+				char buffer[255];
+
+	if(recvfrom(soc,buffer,strlen("hello server"),0,(struct sockaddr*)&receiver,&receiverlen)==-1)
+			err("Nu merge recvfrom");
+
+			pthread_mutex_lock(&mutex);
+			flag1=1;
+			pthread_cond_signal(&flag1_cv);
+			pthread_mutex_unlock(&mutex);
+
+	int i=1;
+	while(1)
+	{
+		if(recvfrom(soc,buffer,strlen("hello server"),0,(struct sockaddr*)&receiver,&receiverlen)==-1)
+			err("Nu merge recvfrom");
+		printf("%s \n",buffer);
+
+
+	}
+}
+void *udpserver()
+{
+	int soc;
+	char buffer[3],mesajclient[10];
+	struct sockaddr_in server,client;
+	   socklen_t slen=sizeof(client);
+
+	if((soc=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP))==-1)
+			err("Nu merge socket");
+			//creare socket server
+	memset((char *) &server,0,sizeof(server));
+	server.sin_family=AF_INET;
+	server.sin_port=htons(5000);
+	inet_aton("255.255.255.255",&server.sin_addr.s_addr);
+
+	client.sin_family=AF_INET;
+	client.sin_port=htons(5000);
+	inet_aton("255.255.255.255",&client.sin_addr.s_addr);
+			// afisare ip server
+
+
+	struct ifreq ifr;
+	char iface[] = "wlx00160a2401d4";
+	ifr.ifr_addr.sa_family = AF_INET;
+	strncpy(ifr.ifr_name , iface , IFNAMSIZ-1);
+	ioctl(soc, SIOCGIFADDR, &ifr);
+	printf("\n Serverul cu ip-adressul : %s si portul %d\n",inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr),ntohs(server.sin_port));
+			//binding
+
+	//if(bind(soc,(struct sockaddr*)&server,sizeof(server))==-1)
+	//	err("Nu merge bind");
+
+			//broadcast
+	int broadcastPermission=1;
+	if (setsockopt(soc, SOL_SOCKET, SO_BROADCAST, (void *) &broadcastPermission,
+	          sizeof(broadcastPermission)) < 0)
+	          err("Eroare la broadcast");
+		//waiting for a client
+	printf("\nAstept client:\n");
+
+	/*if(recvfrom(soc,mesajclient,strlen("Hello Server"),0,(struct sockaddr*)&client,&slen)==-1)
+		err("Recv from error");
+	printf("Received packet from %s : %d \n",inet_ntoa(client.sin_addr),ntohs(client.sin_port));
+	printf("Data received : %s\n",mesajclient);
+*/
+
+	while(flag1==0)
+	{
+		pthread_cond_wait(&flag1_cv,&mutex);
+	}
+
+	while(1)
+		{
+
+		printf("\nAlegeti comanda dorita : \n");
+		fgets(buffer,100,stdin);printf("%s",buffer);
+				if(sendto(soc,buffer,strlen(buffer),0,(struct sockaddr*)&client,slen)==-1)
+					err("Sendto error");
+
+
+
+	}
+	close(soc);
+
+}
 int main (void)
 {
-int soc;
-char buffer[3],mesajclient[10];
-struct sockaddr_in server,client;
-   socklen_t slen=sizeof(client);	
-	
-if((soc=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP))==-1)
-		err("Nu merge socket");
-		//creare socket server
-memset((char *) &server,0,sizeof(server));
-server.sin_family=AF_INET;
-server.sin_port=htons(5000);
-inet_aton("255.255.255.255",&server.sin_addr.s_addr);
-		// afisare ip server
-struct ifreq ifr;
-char iface[] = "wlx00160a2401d4";
-ifr.ifr_addr.sa_family = AF_INET;
-strncpy(ifr.ifr_name , iface , IFNAMSIZ-1);
-ioctl(soc, SIOCGIFADDR, &ifr);
-printf("\n Serverul cu ip-adressul : %s si portul %d\n",inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr),ntohs(server.sin_port));
-		//binding	
-if(bind(soc,(struct sockaddr*)&server,sizeof(server))==-1)
-		err("Nu merge bind");
-		//broadcast
-int broadcastPermission=1;
-if (setsockopt(soc, SOL_SOCKET, SO_BROADCAST, (void *) &broadcastPermission, 
-          sizeof(broadcastPermission)) < 0)
-          err("Eroare la broadcast");
-		//waiting for a client
-printf("\nAstept client:\n");
-if(recvfrom(soc,mesajclient,10,0,(struct sockaddr*)&client,&slen)==-1)
-	err("Recv from error");
-printf("Received packet from %s : %d \n",inet_ntoa(client.sin_addr),ntohs(client.sin_port));
-printf("Data received : %s\n",mesajclient);
-while(1)
-	{
-	printf("\nAlegeti comanda dorita : \n");
-	fgets(buffer,100,stdin);printf("%s",buffer);
-			if(sendto(soc,buffer,strlen(buffer),0,(struct sockaddr*)&client,slen)==-1)
-				err("Sendto error");
-		
-	
+  pthread_mutex_init(&mutex,NULL);
+	pthread_cond_init(&flag1_cv,NULL);
+	pthread_t t1;
+	pthread_create(&t1,NULL,(void*)udpclientreader,NULL);
+	pthread_join(t1,NULL);
 
-}
-close(soc);
+	pthread_t t2;
+	pthread_create(&t2,NULL,(void*)udpserver,NULL);
+	pthread_join(t2,NULL);
+
 return 0;
 }
-
